3ceef79f4e6d85bc59c0a6a772447077fc4d6b68
Yongmin0423 <yym21345@gmail.com>
2024-11-10 00:51:03 +0900

    load spinner

--- src/page/ProductDetailPage/ProductDetailPage.js
+++ src/page/ProductDetailPage/ProductDetailPage.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Container, Row, Col, Button, Dropdown } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Dropdown,
+  Spinner,
+} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { ColorRing } from "react-loader-spinner";
 import { currencyFormat } from "../../utils/number";
@@ -48,15 +55,12 @@ const ProductDetail = () => {
 
   if (loading || !selectedProduct)
     return (
-      <ColorRing
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="blocks-loading"
-        wrapperStyle={{}}
-        wrapperClass="blocks-wrapper"
-        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
-      />
+      <div className="loader-container">
+        <Col className="text-align-center">
+          <Spinner animation="border" />
+          <h5>Loading...</h5>
+        </Col>
+      </div>
     );
   return (
     <Container className="product-detail-card">
--- src/page/UserDetail/UserDetailPage.js
+++ src/page/UserDetail/UserDetailPage.js
@@ -62,14 +62,16 @@ const UserDetailPage = () => {
 
   if (loading) {
     return (
-      <ColorRing
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="blocks-loading"
-        wrapperClass="blocks-wrapper"
-        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
-      />
+      <div className="loader-container">
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperClass="blocks-wrapper"
+          colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+        />
+      </div>
     );
   }
 
